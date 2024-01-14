9caa8c40cf8c25b856e791c6b38badf5a273c710
ArthurPaes <arthur.sldp@gmail.com>
2024-01-14 09:00:25 -0300

    feat: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../screens/HomePage';
+import MovieDetails from '../screens/MovieDetails';
 
 const Rotas: React.FC = () => (
   <Routes>
     <Route path="/" Component={HomePage} />
+    <Route path="/filmes/:id" Component={MovieDetails} />
   </Routes>
 );
 
