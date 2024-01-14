a36963c7c911679fffbb25291f3d48d92805ad86
ArthurPaes <arthur.sldp@gmail.com>
2024-01-13 21:58:02 -0300

    chore: adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#031d33',
+        'light-blue': '#01B4E4',
+        gray: '#666666',
+        'light-gray': '#d4d4d4',
+        green: '#21d07a',
+        'dark-green': '#204529',
+        yellow: '#d2d531',
+        'dark-yellow': '#423d0f',
+        red: '#db2360',
+        'dark-red': '#571435',
+      },
+    },
+  },
+  plugins: [],
+};
