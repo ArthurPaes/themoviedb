6038c7d7d5df1e908e32a6725d8b31cb1e6aa559
ArthurPaes <arthur.sldp@gmail.com>
2024-01-15 12:00:27 -0300

    chore: configs and deleting unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import './App.css';
 import './styles/globalStyles.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { BrowserRouter } from 'react-router-dom';
-import Rotas from './routes';
+import AppRoutes from './routes';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer />
       <Header />
-      <Rotas />
+      <AppRoutes />
       <Footer />
     </BrowserRouter>
   );
